c1ba6c2663f789fb5f7f60a99a443e6850869300
yukihyoto <yhyoto@gmail.com>
2016-05-18 08:45:15 +0900

    Update nec2kosma.py

--- nec2kosma.py
+++ nec2kosma.py
@@ -2,11 +2,23 @@
 
 import time
 import controller.py
+
+
+#function 
+def read_cookie():
+  pass
  
+ def write_cookie():
+  pass
+ 
+
+
 
 #main loop
 if __name__ == "__main__"
 while 1
+
+
 time.sleep(1)
 
 
