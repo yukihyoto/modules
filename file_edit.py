51984a916f3340893336f85c52c2d5432ae98dac
yukihyoto <yhyoto@gmail.com>
2016-07-06 10:37:27 +0900

    Update file_edit.py

--- file_edit.py
+++ file_edit.py
@@ -6,15 +6,16 @@ if __name__ == "__main__" :
   
   # 6000 point = 10 min, 864000 point = 1 day, 2592000 point = 3 days
   length = 2892000
-  # 6000  
-  for i in length :
+  
+  # 6000
+  for i in range(length) :
     line = f.readline()
     if i%6000 == 0 :
       flag = 1
     else :
       flag = 0
     
-    if flag = 1 :
+    if flag == 1 :
       newfile.write(line)
       flag = 0
     else :
